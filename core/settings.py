86ab4e8af8925b317225579b69eff6b311a3e320
Jon Clayton <m87tech@gmail.com>
2021-05-05 17:56:26 +0100

    latest

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,15 @@
 import json
 from pathlib import Path
+from os import environ
+import os
+
+
 
 with open(str(Path("data/settings.json"))) as f:
   config = json.load(f)
+
+# Over-ride via environment vars
+if os.getenv("JIMI_DB") is not None:
+  config['mongodb']['hosts'][0] = os.getenv("JIMI_DB")
+
 
