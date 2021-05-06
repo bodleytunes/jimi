120dd179c496f7ed46dde33890ac87567eb34f6e
Jon Clayton <m87tech@gmail.com>
2021-05-06 14:58:34 +0100

    modified docker settings

--- core/settings.py
+++ core/settings.py
@@ -8,8 +8,6 @@ import os
 with open(str(Path("data/settings.json"))) as f:
   config = json.load(f)
 
-# Over-ride via environment vars
-if os.getenv("JIMI_DB") is not None:
-  config['mongodb']['hosts'][0] = os.getenv("JIMI_DB")
+
 
 
